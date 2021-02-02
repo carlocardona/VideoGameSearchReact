cd1cfb89dbe0fcc32bc7140605e2a530d2388125
Carlo C <pbjunkiep3@yahoo.com>
2021-02-02 11:58:04 +1100

    added state management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +25,16 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// const initState = {
+//   name: '',
+//   isLogged: false,
+// }
+
+// const userReducer = ( state = initState,action ) => {
+//   switch(action.type){
+//       default:
+//           return {...state};
+//   }
+// }
---
+++ src/reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import gamesReducer from './gamesReducer';
+
+const rootReducer = combineReducers({
+    games: gamesReducer,
+    user: userReducer,
+})
+
+export default rootReducer;
